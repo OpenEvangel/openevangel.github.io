7bd1239fa654d0dc152745b72af2661d1376725a
Rich Zhao <zhao.zhenhua@gmail.com>
2023-03-05 00:23:15 +0800

    change pig position

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import styles from '../index.module.css';
+import clsx from 'clsx';
 
 export default function Events() {
   return (
@@ -13,7 +17,13 @@ export default function Events() {
           height: '50vh',
           fontSize: '20px',
         }}>
-        <Link to="/events/20230305">开源合规安全与艺术</Link>
+        {/* <h1 className='hero__title'>最近活动</h1> */}
+        <ul>
+          <li>
+            <Link to="/events/20230305">开源合规安全与艺术</Link>
+          </li>
+        </ul>
+
       </div>
     </Layout>
   );
