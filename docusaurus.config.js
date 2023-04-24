eaa64a137f6709b14e850fc192275621b2ece4fd
Rich Zhao <zhao.zhenhua@gmail.com>
2023-04-24 16:11:44 +0800

    config - ignore when detects broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'openevangel.com', // Usually your GitHub org/user name.
   projectName: 'openevangel', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
